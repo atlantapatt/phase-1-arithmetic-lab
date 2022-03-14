a3206589f34e71808aa631ce9c3c48539222b52b
AtlantaPatterson <atlantarpatterson@gmail.com>
2022-03-14 15:29:28 -0700

    commit tree cf50cb576591a31264a305fa7ee8c24ab59ed096

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code here //
+const num1 = 2
+const num2 = 31
+let multiply = (num1 * num2)
+
+let random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 4
+const num4 = 5
+let mod = num3 % num4
+
+const num5 = Math.max(10, 11, 20)
+let max = num5
